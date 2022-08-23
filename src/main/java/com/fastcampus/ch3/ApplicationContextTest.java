1371fc7f91ea4e2c1a07681084daded241c212ce
lim950808 <lim080895@gmail.com>
2022-08-24 01:54:07 +0900

    Spring DI 활용하기 - 이론(4)

--- src/main/java/com/fastcampus/ch3/ApplicationContextTest.java
+++ src/main/java/com/fastcampus/ch3/ApplicationContextTest.java
@@ -46,6 +46,32 @@ class Car {
     }
 }
 
+@Component
+@PropertySource("setting.properties")
+class SysInfo {
+    @Value("#{systemProperties['user.timezone']}")
+    String timeZone;
+    @Value("#{systemEnvironment['APPDATA']}")
+    String currDir;
+    @Value("${autosaveDir}")
+    String autosaveDir;
+    @Value("${autosaveInterval}")
+    int autosaveInterval;
+    @Value("${autosave}")
+    boolean autosave;
+
+    @Override
+    public String toString() {
+        return "SysInfo{" +
+                "timeZone='" + timeZone + '\'' +
+                ", currDir='" + currDir + '\'' +
+                ", autosaveDir='" + autosaveDir + '\'' +
+                ", autosaveInterval=" + autosaveInterval +
+                ", autosave=" + autosave +
+                '}';
+    }
+}
+
 public class ApplicationContextTest {
     public static void main(String[] args) {
         ApplicationContext ac = new GenericXmlApplicationContext("config.xml");
@@ -53,6 +79,14 @@ public class ApplicationContextTest {
         Car car  = (Car) ac.getBean("car"); // 이름으로 빈 검색
         Car car2 = (Car) ac.getBean(Car.class);   // 타입으로 빈 검색
         System.out.println("car = " + car);
+
+        System.out.println("ac.getBean(SysInfo.class) = " + ac.getBean(SysInfo.class));
+        Map<String, String> map = System.getenv();
+        System.out.println("map = " + map);
+
+        Properties properties = System.getProperties();
+        System.out.println("properties = " + properties);
+
 //        System.out.println("car2 = " + car2);
 
 //        System.out.println("ac.getBeanDefinitionNames() = " + Arrays.toString(ac.getBeanDefinitionNames())); // 정의된 빈의 이름을 배열로 반환
